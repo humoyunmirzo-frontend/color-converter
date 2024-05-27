86889620337a295132e035fc13eb8828dbb8e3f5
Humoyunmirzo Xoliqberdiyev <111569832+humoyunmirzo-frontend@users.noreply.github.com>
2024-05-27 20:13:05 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const colorInput = document.getElementById('color-input')
 const converterResult = document.getElementById('converter-result')
-console.log(1)
 function converter(){
-    console.log(12)
     converterResult.style.background = `#${colorInput.value}`
 }
